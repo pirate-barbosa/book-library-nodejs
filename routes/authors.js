81528437742d282bcb2c8f804a9f2d2020dd2910
Sampath Paravasthu <write2sampath@gmail.com>
2019-12-12 01:27:08 +0530

    Finished author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 // Get all Authors
 router.get('/', async (req, res) => {
@@ -21,7 +22,10 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-  res.render('authors/new', { author: new Author() });
+  let locals = {
+    author: new Author()
+  };
+  res.render('authors/new', locals);
 });
 
 router.post('/', async (req, res) => {
@@ -31,8 +35,7 @@ router.post('/', async (req, res) => {
 
   try {
     const newAuthor = await author.save();
-    // res.redirect(`authors/${newAuthor.id}`);
-    res.redirect('authors');
+    res.redirect(`authors/${newAuthor.id}`);
   } catch {
     let locals = {
       author: author,
@@ -42,4 +45,63 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id })
+      .limit(6)
+      .exec();
+    let locals = {
+      author,
+      booksByAuthor: books
+    };
+    res.render('authors/show', locals);
+  } catch {
+    res.redirect('/');
+  }
+});
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render('authors/edit', { author: author });
+  } catch {
+    res.redirect('/authors');
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect('/');
+    } else {
+      let locals = {
+        author: author,
+        errorMessage: 'Error Updating Author'
+      };
+      res.render('authors/edit', locals);
+    }
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect(`/authors`);
+  } catch {
+    if (author == null) {
+      res.redirect('/');
+    } else {
+      res.redirect(`/authors/${author.id}`);
+    }
+  }
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -16,10 +17,13 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-  limit: '10mb',
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    limit: '10mb',
+    extended: false
+  })
+);
+app.use(methodOverride('_method'));
 
 mongoose.set('useUnifiedTopology', true);
 // mongoose.set('debug', true);
